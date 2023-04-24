48d29056dfcd95a8754edfb5ba78e1c20e0cb1f4
jhansi9618 <jhansi.marolix@gmail.com>
2023-04-24 11:42:35 +0530

    friday project

---
+++ todo_project/todoapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'index.html', context)
